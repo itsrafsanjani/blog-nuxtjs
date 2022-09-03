30a0e3d3853bee7402c1532c674ecdfd9c1975b3
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-09-03 22:02:04 +0600

    feat: posts.index and posts.show page added, dynamic wip

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container p-3">
+    <h3 class="text-center">Lorem ipsum dolor sit amet.</h3>
+    <div class="mt-3 bg-gray-200">
+      <p v-for="(paragraph, index) in 10" :key="index" class="bg-gray-100 mb-2 p-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        doloribus temporibus culpa perspiciatis possimus aliquam itaque ea
+        repudiandae maxime ex? Molestiae vero ipsam ad atque harum blanditiis
+        eveniet est nobis.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PostShow",
+});
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="text-center">
-    Posts
+  <div class="container p-3">
+    <h3 class="text-center">Posts</h3>
+    <ul class="mt-3">
+      <li
+        v-for="(post, index) in 10"
+        :key="index"
+        class="mb-3 p-3 rounded text-white bg-indigo-400"
+      >
+        <NuxtLink :to="{ name: 'posts-slug', params: { slug: index } }">
+          <h1 class="text-xl mb-2">Lorem ipsum dolor sit amet.</h1>
+          <p class="truncate">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, nobis
+            voluptatibus? Illum, suscipit. Rerum aspernatur soluta
+            necessitatibus voluptate quasi error similique nihil voluptatem
+            provident minima, inventore molestiae ipsam eum illum.
+          </p>
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'PostIndex'
-})
+  name: "PostIndex",
+});
 </script>
